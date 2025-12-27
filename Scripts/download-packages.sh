0d9101fc5a3ee57ea642c23cd498f925e2b55625
stefan <stefan@ft-linux.ft-linux.virtualbox.org>
2025-12-28 00:37:36 +0100

    Use Oregon State University Open Source Lab (osuosl) Mirror for packages

--- Scripts/download-packages.sh
+++ Scripts/download-packages.sh
@@ -3,7 +3,7 @@
 rm -rf $LFS/sources/*
 wget --input-file=Resources/wget-list --continue --directory-prefix=$LFS/sources
 
-cp Resources/md5sums $LFS/md5sums
+cp Resources/md5sums $LFS/sources/md5sums
 
 pushd $LFS/sources
 md5sum -c md5sums
